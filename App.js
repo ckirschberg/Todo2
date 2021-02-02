df704ada5bc922fa78a4b7d22b41e546eb9b9e8c
Christian Kirschberg <ckirschberg@gmail.com>
2021-02-02 15:16:38 +0100

    .

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [myTodos, setTodo] = useState([]);
+
+  // Function definition - (enteredText is variable name of local var.)
+  const todoInputHandler = (enteredText) => {
+    setNewTodo(enteredText); // Calling 'setter'(function) defined on line 6.
+  };
+  const addTodoHandler = () => {
+    setTodo(currentTodos => [...currentTodos, newTodo]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder="Add new todo" value={newTodo}
+        onChangeText={todoInputHandler} />
+      
+      <Button title="Add Todo" onPress={addTodoHandler}></Button>
+
+      {/* <FlatList /> */}
     </View>
   );
 }
