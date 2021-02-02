f0f3972702b50c51585075204f313db9e32383f4
Christian Kirschberg <ckirschberg@gmail.com>
2021-02-02 15:22:39 +0100

    comments

--- App.js
+++ App.js
@@ -10,7 +10,13 @@ export default function App() {
   const todoInputHandler = (enteredText) => {
     setNewTodo(enteredText); // Calling 'setter'(function) defined on line 6.
   };
+  
+  // When the user clicks the button, this function is called.
   const addTodoHandler = () => {
+    // This is setting the myTodos variable in the state, as it sets it, it retrieves
+    // a snapshot (it is a function), and the following: [...currentTodos, newTodo] will
+    // read the array and create a new array and copy the existing values into the new array
+    // and the second parameter (newTodo) will be added to the new array.
     setTodo(currentTodos => [...currentTodos, newTodo]);
   };
 
